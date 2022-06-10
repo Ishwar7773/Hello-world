715bce1e2aa2c2b88149dcc6512ec4b3a488dadc
ishwar7773 <ishwarshekhawat850@gmail.com>
2022-06-10 10:02:12 +0530

    Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/com/ishwar/App.java
@@ -0,0 +1,24 @@
+package com.ishwar;
+
+
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+/*
+ * 
+ * Added Name function
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+    	System.out.print( "Enter your name:" );
+    	String name = scan.next();
+    	System.out.print("Hello "+name);
+    }
+}
---
+++ src/test/java/com/ishwar/AppTest.java
@@ -0,0 +1,15 @@
+package com.ishwar;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
